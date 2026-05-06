3ff633aec28af1ee81a1d35bd1dea46475f80193
cfardin <cfardin51@gmail.com>
2026-05-06 13:52:37 +0600

    added the card section in the home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,8 +1,12 @@
+import HeroBanner from '@/component/HeroBanner';
+import NumberCards from '@/component/NumberCards';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div>
+            <HeroBanner></HeroBanner>
+            <NumberCards></NumberCards>
             <h1>This is home page</h1>
         </div>
     );
--- src/app/layout.js
+++ src/app/layout.js
@@ -27,9 +27,8 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        
+
         <Navbar></Navbar>
-        <HeroBanner></HeroBanner>
         {children}
         <Footer></Footer>
       </body>
