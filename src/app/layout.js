8a5d8dcfb9112948f3d0f8b133f44dd8647398f4
cfardin <cfardin51@gmail.com>
2026-05-07 23:37:15 +0600

    added data-theme to light

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      // data-theme = "light"
+      data-theme = "light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
